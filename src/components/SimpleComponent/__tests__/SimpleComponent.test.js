97575f1bfae0b92ecb1276b107dc576cbc2e522c
Ricardo Moura Vargas <ricardo.mouravargas@intive.com>
2022-06-24 06:42:04 -0300

    adding jest and react testing library

---
+++ src/components/SimpleComponent/__tests__/SimpleComponent.test.js
@@ -0,0 +1,19 @@
+// WORK IN PROGRESS
+// import React from 'react';
+// import '@testing-library/jest-dom';
+// import { render } from '@testing-library/react';
+
+// import SimpleComponent from '../SimpleComponent';
+
+// const Wraper = ({ children }) => <div>{children}</div>;
+
+// test('Check if match with snapshot', () => {
+//     const simpleComponent = render(
+//         <Wraper children={<SimpleComponent colorClass='title-red' />} />
+//     );
+//     expect(simpleComponent).toMatchSnapshot();
+// });
+
+test('SimpleComponent dummy teste', () => {
+    return undefined;
+});
---
+++ src/pages/HomePage/__tests__/HomePage.test.js
@@ -0,0 +1,3 @@
+test('HomePage dummy teste', () => {
+    return undefined;
+});
