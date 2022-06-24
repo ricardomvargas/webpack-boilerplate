03f2ef5e407b2e0fe3057785a06b377d140b77fc
Ricardo Moura Vargas <ricardo.mouravargas@intive.com>
2022-06-24 15:37:17 -0300

    feat: Jest and testing library configured, added basic tests

--- src/components/SimpleComponent/__tests__/SimpleComponent.test.js
+++ src/components/SimpleComponent/__tests__/SimpleComponent.test.js
@@ -1,18 +1,16 @@
-// WORK IN PROGRESS
-// import React from 'react';
-// import '@testing-library/jest-dom';
-// import { render } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 
-// import SimpleComponent from '../SimpleComponent';
+import SimpleComponent from '../SimpleComponent';
 
-// const Wraper = ({ children }) => <div>{children}</div>;
+const Wraper = ({ children }) => <div>{children}</div>;
 
-// test('Check if match with snapshot', () => {
-//     const simpleComponent = render(
-//         <Wraper children={<SimpleComponent colorClass='title-red' />} />
-//     );
-//     expect(simpleComponent).toMatchSnapshot();
-// });
+test('Check if match with snapshot', () => {
+    const simpleComponent = render(
+        <Wraper children={<SimpleComponent colorClass='title-red' />} />
+    );
+    expect(simpleComponent).toMatchSnapshot();
+});
 
 test('SimpleComponent dummy teste', () => {
     return undefined;
